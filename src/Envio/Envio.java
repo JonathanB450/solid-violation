b43c41a13b16ff6025dba239560a571596d71037
Jonathan Buitrago <jonathanbuitragoroncancio50@gmail.com>
2022-07-23 22:26:57 -0500

    Update Envio.java

--- src/Envio/Envio.java
+++ src/Envio/Envio.java
@@ -9,11 +9,13 @@ public class Envio{
   public Envio(){
     this.tipo = "";
     this.precio = 0;
+    this.tiempoEnvio = 0;
   }
 
-  public Envio(String tipo, Integer precio) {
+  public Envio(String tipo, Integer precio, Integer tiempoEnvio) {
     this.tipo = tipo;
     this.precio = precio;
+    this.tiempoEnvio = tiempoEnvio;
   }
 
   public String getTipo() {
